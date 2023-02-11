b41d103c02e19f8a9f5c1cda59368f2db03c78b9
Springcomp <springcomp@users.noreply.github.com>
2023-02-11 10:01:06 +0100

    Updated to .NET 6.0

--- src/Compliance.cs
+++ src/Compliance.cs
@@ -1,9 +1,64 @@
-namespace src
+public sealed class Compliance
 {
-    public sealed class Compliance
-    {
-        public int Total { get; set; }
-        public int Succeeded { get; set; }
-        public Diff[] Diffs { get; set; }
-    }
+	public int Total { get; set; } = default!;
+	public int Succeeded { get; set; } = default!;
+	public Diff[] Diffs { get; set; } = Array.Empty<Diff>();
+
+	public Compliance Merge(Compliance other)
+	=> Compliance.Merge(this, other);
+
+	public static Compliance CompareLines(int lineNum, string actual, string expected)
+	{
+		var length = Math.Min(actual.Length, expected.Length);
+		var succeeded = 0;
+		var characters = 0;
+
+		var diffs = new List<Diff>();
+
+		// TODO: improve alg if line lengths are different
+
+		for (var column = 0; column < length; column++)
+		{
+			if (actual[column] == expected[column])
+			{
+				if (actual[column] != ' ')
+				{
+					succeeded++;
+					characters++;
+				}
+			}
+
+			else
+			{
+				characters++;
+
+				diffs.Add(new Diff
+				{
+					LineNumber = lineNum,
+					ColumnNumber = column + 1,
+					Actual = actual[column],
+					Expected = expected[column],
+				});
+			}
+		}
+
+		return new Compliance
+		{
+			Total = characters,
+			Succeeded = succeeded,
+			Diffs = diffs.ToArray(),
+		};
+	}
+	private static Compliance Merge(Compliance left, Compliance right)
+	{
+		left.Total += right.Total;
+		left.Succeeded += right.Succeeded;
+
+		var arr = new List<Diff>(left.Diffs ?? new Diff[] { });
+		arr.AddRange(right.Diffs ?? new Diff[] { });
+
+		left.Diffs = arr.ToArray();
+
+		return left;
+	}
 }
--- src/Diff.cs
+++ src/Diff.cs
@@ -1,12 +1,7 @@
-using System;
-
-namespace src
+public sealed class Diff
 {
-    public sealed class Diff
-    {
-        public int LineNumber { get; set; }
-        public int ColumnNumber { get; set; }
-        public Char Expected { get; set; }
-        public Char Actual { get; set; }
-    }
+	public int LineNumber { get; set; } = default!;
+	public int ColumnNumber { get; set; } = default!;
+	public Char Expected { get; set; } = default!;
+	public Char Actual { get; set; } = default!;
 }
